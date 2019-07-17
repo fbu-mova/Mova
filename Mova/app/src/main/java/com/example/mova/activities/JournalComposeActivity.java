a0b331f60d36b6026d74a16d0e1cf07a3e92d323
Jessie Mindel <mindel.jessie@gmail.com>
2019-07-16 17:51:05 -0700

    Add JournalComposeActivity
    Location is currently hardcoded; mood does not yet work.

--- Mova/app/src/main/java/com/example/mova/activities/JournalComposeActivity.java
+++ Mova/app/src/main/java/com/example/mova/activities/JournalComposeActivity.java
@@ -1,16 +1,174 @@
 package com.example.mova.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.mova.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationServices;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseUser;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class JournalComposeActivity extends AppCompatActivity {
+
+    @BindView(R.id.tvTime)      protected TextView tvTime;
+    @BindView(R.id.tvLocation)  protected TextView tvLocation;
+    @BindView(R.id.etBody)      protected EditText etBody;
+    @BindView(R.id.bSave)       protected Button bSave;
+
+    private GoogleApiClient googleApiClient = new GoogleApiClient() {
+        @Override
+        public boolean hasConnectedApi(@NonNull Api<?> api) {
+            return false;
+        }
+
+        @NonNull
+        @Override
+        public ConnectionResult getConnectionResult(@NonNull Api<?> api) {
+            return null;
+        }
+
+        @Override
+        public void connect() {
+
+        }
+
+        @Override
+        public ConnectionResult blockingConnect() {
+            return null;
+        }
+
+        @Override
+        public ConnectionResult blockingConnect(long l, @NonNull TimeUnit timeUnit) {
+            return null;
+        }
+
+        @Override
+        public void disconnect() {
+
+        }
+
+        @Override
+        public void reconnect() {
+
+        }
+
+        @Override
+        public PendingResult<Status> clearDefaultAccountAndReconnect() {
+            return null;
+        }
+
+        @Override
+        public void stopAutoManage(@NonNull FragmentActivity fragmentActivity) {
+
+        }
+
+        @Override
+        public boolean isConnected() {
+            return false;
+        }
+
+        @Override
+        public boolean isConnecting() {
+            return false;
+        }
+
+        @Override
+        public void registerConnectionCallbacks(@NonNull ConnectionCallbacks connectionCallbacks) {
+
+        }
+
+        @Override
+        public boolean isConnectionCallbacksRegistered(@NonNull ConnectionCallbacks connectionCallbacks) {
+            return false;
+        }
+
+        @Override
+        public void unregisterConnectionCallbacks(@NonNull ConnectionCallbacks connectionCallbacks) {
+
+        }
+
+        @Override
+        public void registerConnectionFailedListener(@NonNull OnConnectionFailedListener onConnectionFailedListener) {
+
+        }
+
+        @Override
+        public boolean isConnectionFailedListenerRegistered(@NonNull OnConnectionFailedListener onConnectionFailedListener) {
+            return false;
+        }
+
+        @Override
+        public void unregisterConnectionFailedListener(@NonNull OnConnectionFailedListener onConnectionFailedListener) {
+
+        }
+
+        @Override
+        public void dump(String s, FileDescriptor fileDescriptor, PrintWriter printWriter, String[] strings) {
+
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_journal_compose);
+        ButterKnife.bind(this);
+
+        Date startDate = new Date();
+        final double lat = 0, lon = 0; // TODO: Get location
+
+        SimpleDateFormat timeFmt = new SimpleDateFormat("h:mm aa", Locale.US);
+
+        tvTime.setText(timeFmt.format(startDate));
+        tvLocation.setText("Seattle, WA, USA"); // TODO: Get location
+        // TODO: Build and handle mood selection
+
+        bSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String body = etBody.getText().toString();
+//                Media media = null; // TODO: Get embedded media
+                ParseUser user = ParseUser.getCurrentUser();
+                Date endDate = new Date();
+                ParseGeoPoint location = new ParseGeoPoint();
+                location.setLatitude(lat);
+                location.setLongitude(lon);
+
+                // TODO: Create journal post
+            }
+        });
     }
 }
