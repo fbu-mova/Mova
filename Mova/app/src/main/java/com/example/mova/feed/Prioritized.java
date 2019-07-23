ef46d005af146ff01b7f46a3c6e6e02e509182c3
Jessie Mindel <mindel.jessie@gmail.com>
2019-07-23 12:45:20 -0700

    Update Prioritized<T>.compareTo to use hashes
    This is done so that we can use HashableParseObject's hashes to conveniently compare equal ParseObjects.

--- Mova/app/src/main/java/com/example/mova/feed/Prioritized.java
+++ Mova/app/src/main/java/com/example/mova/feed/Prioritized.java
@@ -1,5 +1,10 @@
 package com.example.mova.feed;
 
+/**
+ * An item with an associated integer value to signify its priority.
+ * Items must have unique hash codes for proper comparison if items are equal.
+ * @param <T> The type of item with which to associate a value.
+ */
 public class Prioritized<T> implements Comparable<Prioritized<T>> {
     public final T item;
     public final float value;
@@ -13,8 +18,18 @@ public class Prioritized<T> implements Comparable<Prioritized<T>> {
     @Override
     public int compareTo(Prioritized<T> o) {
         // FIXME: This very well could rely on the subtraction of the floats instead, but this works for now.
-        if (value < o.value)       return -1;
-        else if (value == o.value) return 0;
-        return 1;
+        if (value < o.value) {
+            return -1;
+        } else if (value > o.value) {
+            return 1;
+        }
+        // If objects are equal, compare their hash codes
+        // T should not have to implement Comparable<T>, since Prioritized<T> acts as the main comparison point
+        return Integer.compare(item.hashCode(), o.item.hashCode());
+    }
+
+    @Override
+    public int hashCode() {
+        return item.hashCode();
     }
 }
