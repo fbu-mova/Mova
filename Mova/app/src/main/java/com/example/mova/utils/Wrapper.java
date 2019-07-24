ad9b82e2f9d4eeb0315da290b598eb5a97ef28af
Jessie Mindel <mindel.jessie@gmail.com>
2019-07-24 15:53:21 -0700

    Add Wrapper documentation

--- Mova/app/src/main/java/com/example/mova/utils/Wrapper.java
+++ Mova/app/src/main/java/com/example/mova/utils/Wrapper.java
@@ -1,5 +1,9 @@
 package com.example.mova.utils;
 
+/**
+ * Stores one item. Intended for use in arrow function contexts, where an item must be final to be referenced in an internal closure (but this item doesn't have to be final).
+ * @param <T> The type of item to store.
+ */
 public class Wrapper<T> {
     public T item;
 }
