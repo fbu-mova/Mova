99648b478440e881b413fc813ddc4a46a2f599dd
Jessie Mindel <mindel.jessie@gmail.com>
2019-07-26 16:10:49 -0700

    Add ComposeMediaComponent ViewHolder, abstract methods

--- Mova/app/src/main/java/com/example/mova/components/ComposeMediaComponent.java
+++ Mova/app/src/main/java/com/example/mova/components/ComposeMediaComponent.java
@@ -1,47 +1,86 @@
 package com.example.mova.components;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 
+import com.example.mova.R;
 import com.example.mova.activities.DelegatedResultActivity;
+import com.example.mova.model.Media;
+import com.example.mova.scrolling.EndlessScrollRefreshLayout;
 
-public class ComposeMediaComponent extends Component {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public abstract class ComposeMediaComponent extends Component {
+
+    private DelegatedResultActivity activity;
+    private ViewHolder holder;
+    private View view;
+    private ComponentManager manager;
+
+    public ComposeMediaComponent() {
+
+    }
+
     @Override
     public void makeViewHolder(DelegatedResultActivity activity, ViewGroup parent, boolean attachToRoot) {
-
+        this.activity = activity;
+        LayoutInflater inflater = activity.getLayoutInflater();
+        view = inflater.inflate(R.layout.dialog_compose_media, parent, attachToRoot);
+        holder = new ViewHolder(view);
     }
 
     @Override
     public ViewHolder getViewHolder() {
-        return null;
+        return holder;
     }
 
     @Override
     public View getView() {
-        return null;
+        return view;
     }
 
     @Override
     public String getName() {
-        return null;
+        return "ComposeMedia";
     }
 
     @Override
     public void setManager(ComponentManager manager) {
-
+        this.manager = manager;
     }
 
     @Override
     public void render() {
-
+        // TODO
     }
+
+    public abstract void onSelectMedia(Media media);
+    public abstract void onBack();
+    public abstract void onCancel();
 
     public static class ViewHolder extends Component.ViewHolder {
 
+        @BindView(R.id.llButtons)     public LinearLayout llButtons;
+        @BindView(R.id.ivBack)        public ImageView ivBack;
+        @BindView(R.id.ivClose)       public ImageView ivClose;
+
+        @BindView(R.id.llPhotos)      public LinearLayout llPhotos;
+        @BindView(R.id.cvCamera)      public CardView cvCamera;
+        @BindView(R.id.ivCamera)      public ImageView ivCamera;
+        @BindView(R.id.cvLibrary)     public CardView cvLibrary;
+
+        @BindView(R.id.esrlScrapbook) public EndlessScrollRefreshLayout<Component.ViewHolder> esrlScrapbook;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
 }
