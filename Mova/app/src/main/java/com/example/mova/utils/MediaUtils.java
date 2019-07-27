8a12dcf044120e0d951a1ad3e9e369c6bbc5de02
Ashley Chen <ashley.j.chen@gmail.com>
2019-07-26 17:25:07 -0700

    Create MediaUtils

---
+++ Mova/app/src/main/java/com/example/mova/utils/MediaUtils.java
@@ -0,0 +1,48 @@
+package com.example.mova.utils;
+
+import android.util.Log;
+
+import com.example.mova.model.Media;
+import com.example.mova.model.Post;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
+
+public class MediaUtils {
+
+    public static final String TAG = "MediaUtils";
+
+    public static void updateMediaToPost(Post parentPost, ParseObject child, AsyncUtils.ItemCallback<ParseObject> callback) {
+
+        // create Media
+        Media childMedia = new Media()
+                .setParent(parentPost)
+                .setContent(child);
+
+        // save childMedia
+        childMedia.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d(TAG, "saved child media");
+                    parentPost.setMedia(childMedia).saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Log.d(TAG, "saved child media to parent post");
+                            }
+                            else {
+                                Log.e(TAG, "failed saving child media to parent post", e);
+                            }
+                            callback.call(parentPost);
+                        }
+                    });
+                }
+                else {
+                    Log.e(TAG, "failed saving child media");
+                    callback.call(parentPost);
+                }
+            }
+        });
+    }
+}
