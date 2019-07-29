fa46bd50c3adaf093faf71af13d44d8b1ddd6e20
Ashley Chen <ashley.j.chen@gmail.com>
2019-07-29 13:31:56 -0700

    Auto stash before merge of "master" and "origin/master"

--- Mova/app/src/main/java/com/example/mova/ConfirmShareGoalDialog.java
+++ Mova/app/src/main/java/com/example/mova/ConfirmShareGoalDialog.java
@@ -29,6 +29,7 @@ import com.example.mova.utils.AsyncUtils;
 import com.example.mova.utils.GroupUtils;
 import com.example.mova.utils.MediaUtils;
 import com.parse.FindCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
@@ -236,7 +237,6 @@ public class ConfirmShareGoalDialog extends DialogFragment {
     private void updatePostMedia(Post post, Group group) {
         // creates media instance to store goal, then put back in post and save
 
-        // FIXME: This previously used a ParseObject--not sure what it was doing.
         MediaUtils.updateMediaToPost(post, goal, new AsyncUtils.ItemCallback<Object>() {
             @Override
             public void call(Object item) {
