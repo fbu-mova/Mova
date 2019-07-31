72f6078e06ab8dfea5903f48bf5a41604473fccc
Ashley Chen <ashley.j.chen@gmail.com>
2019-07-30 18:06:25 -0700

    Created UninvolvedSharedActionComponent

--- Mova/app/src/main/java/com/example/mova/components/UninvolvedSharedActionComponent.java
+++ Mova/app/src/main/java/com/example/mova/components/UninvolvedSharedActionComponent.java
@@ -1,4 +1,38 @@
 package com.example.mova.components;
 
-public class UninvolvedSharedActionComponent {
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mova.activities.DelegatedResultActivity;
+
+public class UninvolvedSharedActionComponent extends Component {
+    @Override
+    public void makeViewHolder(DelegatedResultActivity activity, ViewGroup parent, boolean attachToRoot) {
+
+    }
+
+    @Override
+    public ViewHolder getViewHolder() {
+        return null;
+    }
+
+    @Override
+    public View getView() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public void setManager(ComponentManager manager) {
+
+    }
+
+    @Override
+    public void render() {
+
+    }
 }
