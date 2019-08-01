78a80d7c8c51ab245314dba20856bb57901e2c18
Brandon Reid <sytrick00@gmail.com>
2019-08-01 16:36:52 -0700

    Merging to master

---
+++ Mova/app/src/main/java/com/example/mova/activities/SearchActivity.java
@@ -0,0 +1,18 @@
+package com.example.mova.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.mova.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
