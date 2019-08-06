b24d8dce3096d34762e5674c15dc850c71b8ceb3
Jessie Mindel <mindel.jessie@gmail.com>
2019-08-05 22:48:39 -0700

    Add initial impl of SimpleListAddComp, vertically center RecurSetting

--- Mova/app/src/main/java/com/example/mova/components/SimpleListAddComponent.java
+++ Mova/app/src/main/java/com/example/mova/components/SimpleListAddComponent.java
@@ -3,18 +3,23 @@ package com.example.mova.components;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
+import com.example.mova.R;
 import com.example.mova.activities.DelegatedResultActivity;
 import com.example.mova.component.Component;
 import com.example.mova.component.ComponentManager;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SimpleListAddComponent extends Component {
+public abstract class SimpleListAddComponent extends Component {
 
-    private static int LAYOUT_RES; // TODO
+    private static int LAYOUT_RES = R.layout.component_simple_list_add;
 
     private ViewHolder holder;
     private ComponentManager manager;
@@ -46,7 +51,10 @@ public class SimpleListAddComponent extends Component {
 
     @Override
     protected void onRender(Component.ViewHolder holder) {
+        checkViewHolderClass(holder, ViewHolder.class);
+        this.holder = (ViewHolder) holder;
 
+        this.holder.llRoot.setOnClickListener((v) -> onClick(v));
     }
 
     @Override
@@ -54,7 +62,13 @@ public class SimpleListAddComponent extends Component {
 
     }
 
+    protected abstract void onClick(View view);
+
     public static class ViewHolder extends Component.ViewHolder {
+
+        @BindView(R.id.llRoot) public LinearLayout llRoot;
+        @BindView(R.id.tvAdd)  public TextView tvAdd;
+        @BindView(R.id.ivAdd)  public ImageView ivAdd;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
