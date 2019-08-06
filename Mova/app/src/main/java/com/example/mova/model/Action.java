23b4eb5d59a6e5ca17d8f8a2e4dee423217f0b38
Ashley Chen <ashley.j.chen@gmail.com>
2019-08-05 17:58:35 -0700

    Add Action.Wrapper class to pass info b/w ActionEditComp and GoalCompose

--- Mova/app/src/main/java/com/example/mova/model/Action.java
+++ Mova/app/src/main/java/com/example/mova/model/Action.java
@@ -130,4 +130,34 @@ public class Action extends HashableParseObject {
         put(KEY_IS_PRIORITY, bool);
         return this;
     }
+
+    public static class Wrapper {
+
+        public boolean isPriority;
+        public String message;
+
+        public Wrapper() {
+            this.isPriority = false;
+            this.message = "";
+        }
+
+        public Wrapper setIsPriority(boolean isPriority) {
+            this.isPriority = isPriority;
+            return this;
+        }
+
+        public Wrapper setMessage(String message) {
+            this.message = message;
+            return this;
+        }
+
+        public boolean getIsPriority() {
+            return this.isPriority;
+        }
+
+        public String getMessage() {
+            return this.message;
+        }
+    }
+
 }
