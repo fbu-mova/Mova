3497cf75f25e8564da9f6b2aa188e21f66608213
Jessie Mindel <mindel.jessie@gmail.com>
2019-08-07 20:22:32 -0700

    Update GoalProgressBar

--- Mova/app/src/main/java/com/example/mova/GoalProgressBar.java
+++ Mova/app/src/main/java/com/example/mova/GoalProgressBar.java
@@ -1,12 +1,12 @@
 package com.example.mova;
 
-import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.icu.util.Measure;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
@@ -18,32 +18,26 @@ public class GoalProgressBar extends View {
         // skipped custom indicator type with enums
         // skipped saving instance state (across screen rotations)
 
-    // for the view customizations
+    // Attributes
 
-    // height of the goal indicator -- used to indicate goal completion, may not use (aka goal does not have to be 100% done to be complete)
-    private float goalIndicatorHeight;
-    // thickness of the goal indicator
-    private float goalIndicatorWidth;
-    // bar color when the goal has been reached
-    private int goalReachedColor;
-    // bar color when the goal has not been reached
-    private int goalNotReachedColor;
-    // bar color for the unfilled section (remaining progress)
-    private int unfilledSectionColor;
-    // height of the progress bar
-    private float barHeight;
+    /** Bar color for the filled section (progress completed). */
+    private int filledColor;
+    /** Bar color for the unfilled section (remaining progress). */
+    private int unfilledColor;
+    /** Thickness of the progress bar. */
+    private int thickness;
+    /** Orientation of the progress bar (horizontal = 0, vertical = 1). */
+    private int orientation;
+    /** The maximum length of the progress bar; if the parent container is larger, rounds the bottom. */
+    private int maxLength;
+    /** Whether or not to round the first end of the progress bar. */
+    private boolean roundTop;
 
-    // for coloring/drawing/filling
+    // Animation & progress
+
     private Paint progressPaint;
-
-    // animation to fill progress bar (too much if done for all progress bars?)
     private ValueAnimator barAnimator;
-    private float value;
-
-    // data of a goal
     private int progress;
-    private int goal;
-    private boolean isGoalReached;
 
     public static final int PROGRESS_MAX = 100;
 
@@ -56,22 +50,17 @@ public class GoalProgressBar extends View {
         progressPaint = new Paint();
         progressPaint.setStyle(Paint.Style.FILL_AND_STROKE);
 
-        setGoal(PROGRESS_MAX);
-
         // extract custom attributes
-
         TypedArray typedArray = getContext().getTheme().obtainStyledAttributes(attrs, R.styleable.GoalProgressBar, 0, 0);
 
         try {
             // extract attributes to member variables from typedArray
-            setGoalIndicatorHeight(typedArray.getDimension(R.styleable.GoalProgressBar_goalIndicatorHeight, 10));
-            setGoalIndicatorWidth(typedArray.getDimensionPixelSize(R.styleable.GoalProgressBar_goalIndicatorWidth, 5));
-
-            setGoalReachedColor(typedArray.getColor(R.styleable.GoalProgressBar_goalReachedColor, Color.BLUE));
-            setGoalNotReachedColor(typedArray.getColor(R.styleable.GoalProgressBar_goalNotReachedColor, Color.BLACK));
-            setUnfilledSectionColor(typedArray.getColor(R.styleable.GoalProgressBar_unfilledSectionColor, Color.RED));
-            setBarHeight(typedArray.getDimensionPixelOffset(R.styleable.GoalProgressBar_barHeight, 4));
-
+            setFilledColor(typedArray.getColor(R.styleable.GoalProgressBar_filledColor, getResources().getColor(R.color.blueMid)));
+            setUnfilledColor(typedArray.getColor(R.styleable.GoalProgressBar_unfilledColor, getResources().getColor(R.color.blueUltraLight)));
+            setThickness(typedArray.getDimensionPixelOffset(R.styleable.GoalProgressBar_thickness, getResources().getDimensionPixelSize(R.dimen.elementMargin)));
+            setOrientation(typedArray.getInt(R.styleable.GoalProgressBar_orientation, 1));
+            setMaxLength(typedArray.getInt(R.styleable.GoalProgressBar_maxLength, 0));
+            setRoundTop(typedArray.getBoolean(R.styleable.GoalProgressBar_roundTop, false));
         } finally {
             typedArray.recycle();
         }
@@ -85,87 +74,102 @@ public class GoalProgressBar extends View {
      */
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        // set width to be same width as parent
-        int width = MeasureSpec.getSize(widthMeasureSpec);
+        // Get all relevant information from measure specs
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 
-        // set height -- defined by the goal indicator, which we won't be using
-        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
-        int height;
-        switch (MeasureSpec.getMode(heightMeasureSpec)) {
-            case MeasureSpec.EXACTLY:
-                // we must be exactly the given size
-                height = heightSize;
-                break;
-            case MeasureSpec.AT_MOST:
-                // can't be bigger than specified height
-                height = (int) Math.min(goalIndicatorHeight, heightSize);
-                break;
-            default:
-                // we can be whatever height we want
-                height = (int) goalIndicatorHeight;
-                break;
+        int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int parentHeight = MeasureSpec.getSize(heightMeasureSpec);
+
+        // Set width and height based on measure spec
+        int width, height;
+
+        if (orientation == 0) {
+            width = Math.min(parentWidth, maxLength);
+            height = meetMeasureSpec(heightMode, parentHeight, thickness);
+        } else {
+            height = Math.min(parentHeight, maxLength);
+            width = meetMeasureSpec(widthMode, parentWidth, thickness);
         }
 
         setMeasuredDimension(width, height);
+    }
 
+    protected int meetMeasureSpec(int measureSpecMode, int parent, int desired) {
+        switch (measureSpecMode) {
+            case MeasureSpec.EXACTLY:
+                return parent;
+            case MeasureSpec.AT_MOST:
+                return Math.min(parent, desired);
+            case MeasureSpec.UNSPECIFIED:
+            default:
+                return desired;
+        }
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        int halfHeight = getHeight() / 2;
-        int progressEndX = (int) (getWidth() * progress / 100f);
+        // Set line dimensions
+        int half = (orientation == 0) ? getHeight() / 2: getWidth() / 2;
+        int progressEnd = (orientation == 0) ? (int) (getWidth() * progress / 100f) : (int) (getHeight() * progress / 100f);
 
         // draw the part of the bar that's filled (completed)
-        progressPaint.setStrokeWidth(barHeight);
-        progressPaint.setColor(isGoalReached ? goalReachedColor : goalNotReachedColor);
-        canvas.drawLine(0, halfHeight, progressEndX, halfHeight, progressPaint);
+        progressPaint.setStrokeWidth(thickness);
+        progressPaint.setColor(filledColor);
+
+        if (orientation == 0) {
+            canvas.drawLine(0, half, progressEnd, half, progressPaint);
+        } else {
+            canvas.drawLine(half, 0, half, progressEnd, progressPaint);
+        }
 
         // draw the unfilled section
-        progressPaint.setColor(unfilledSectionColor);
-        canvas.drawLine(progressEndX, halfHeight, getWidth(), halfHeight, progressPaint);
+        progressPaint.setColor(unfilledColor);
 
-        // draw the goal indicator
-        float indicatorPosition = getWidth() * goal / 100f;
-        progressPaint.setColor(goalReachedColor);
-        progressPaint.setStrokeWidth(goalIndicatorWidth);
-        canvas.drawLine(indicatorPosition, halfHeight - goalIndicatorHeight / 2,
-                indicatorPosition, halfHeight + goalIndicatorHeight / 2, progressPaint);
+        if (orientation == 0) {
+            canvas.drawLine(progressEnd, half, getWidth(), half, progressPaint);
+        } else {
+            canvas.drawLine(half, progressEnd, half, getHeight(), progressPaint);
+        }
+
+        // TODO: Add masking on should round
     }
 
-    public void setGoalIndicatorHeight(float goalIndicatorHeight) {
-        this.goalIndicatorHeight = goalIndicatorHeight;
+    public void setFilledColor(int filledColor) {
+        this.filledColor = filledColor;
         invalidate();
     }
 
-    public void setGoalIndicatorWidth(float goalIndicatorWidth) {
-        this.goalIndicatorWidth = goalIndicatorWidth;
+    public void setUnfilledColor(int unfilledColor) {
+        this.unfilledColor = unfilledColor;
         invalidate();
     }
 
-    public void setGoalReachedColor(int goalReachedColor) {
-        this.goalReachedColor = goalReachedColor;
+    public void setThickness(int thickness) {
+        this.thickness = thickness;
         invalidate();
     }
 
-    public void setGoalNotReachedColor(int goalNotReachedColor) {
-        this.goalNotReachedColor = goalNotReachedColor;
+    public void setOrientation(int orientation) {
+        if (!(orientation == 0 || orientation == 1)) {
+            throw new IllegalArgumentException("Orientation must be 0 for horizontal or 1 for vertical; " + orientation + " is not a valid value.");
+        }
+        this.orientation = orientation;
         invalidate();
     }
 
-    public void setUnfilledSectionColor(int unfilledSectionColor) {
-        this.unfilledSectionColor = unfilledSectionColor;
+    public void setMaxLength(int maxLength) {
+        this.maxLength = maxLength;
         invalidate();
     }
 
-    public void setBarHeight(float barHeight) {
-        this.barHeight = barHeight;
+    public void setRoundTop(boolean roundTop) {
+        this.roundTop = roundTop;
         invalidate();
     }
 
     public void setProgress(int progress) {
         this.progress = progress;
-        updateGoalReached();
-
         setProgress(progress, true);
     }
 
@@ -180,12 +184,9 @@ public class GoalProgressBar extends View {
 
             barAnimator.setInterpolator(new DecelerateInterpolator());
 
-            barAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-                @Override
-                public void onAnimationUpdate(ValueAnimator animation) {
-                    float interpolation = (float) animation.getAnimatedValue();
-                    setProgress((int) (interpolation * progress), false);
-                }
+            barAnimator.addUpdateListener((ValueAnimator animation) -> {
+                float interpolation = (float) animation.getAnimatedValue();
+                setProgress((int) (interpolation * progress), false);
             });
 
             if (!barAnimator.isStarted()) {
@@ -196,15 +197,4 @@ public class GoalProgressBar extends View {
             postInvalidate();
         }
     }
-
-    public void setGoal(int goal) {
-        this.goal = goal;
-        updateGoalReached();
-        invalidate();
-    }
-
-    private void updateGoalReached() {
-        isGoalReached = progress >= goal;
-    }
-
 }
