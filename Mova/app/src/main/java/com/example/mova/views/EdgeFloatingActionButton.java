e9b0edee729b1aaddd5df577cb7ef705a7f90b13
Jessie Mindel <mindel.jessie@gmail.com>
2019-08-10 12:32:28 -0700

    Migrate EFAB complete cutoff to EFAB
    All of the widths and heights are hardcoded for now, but it should do the job.

--- Mova/app/src/main/java/com/example/mova/views/EdgeFloatingActionButton.java
+++ Mova/app/src/main/java/com/example/mova/views/EdgeFloatingActionButton.java
@@ -5,30 +5,40 @@ import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Space;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import com.example.mova.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class EdgeFloatingActionButton extends FrameLayout {
 
-    @BindView(R.id.flRoot)  protected FrameLayout flRoot;
-    @BindView(R.id.cvMask)  protected CardView cvMask;
-    @BindView(R.id.flImage) protected FrameLayout flImage;
-    @BindView(R.id.iv)      protected ImageView iv;
+    @BindView(R.id.flRoot)   protected FrameLayout flRoot;
+    @BindView(R.id.cvMask)   protected CardView cvMask;
+    @BindView(R.id.flImage)  protected FrameLayout flImage;
+    @BindView(R.id.iv)       protected ImageView iv;
+
+    @BindView(R.id.clRoot)   protected ConstraintLayout clRoot;
+    @BindView(R.id.flPadded) protected FrameLayout flPadded;
+    @BindView(R.id.space)    protected Space space;
 
     private int cornerRadius;
     private Drawable image;
     private int imageTint, backgroundTint;
     private int edge;
+    private int padding;
 
     private enum Edge {
         Left(1),
@@ -55,6 +65,34 @@ public class EdgeFloatingActionButton extends FrameLayout {
             if (value == Right.value)  return Right;
             if (value == Bottom.value) return Bottom;
             return null;
+        }
+
+        public int constraintSide() {
+            switch (this) {
+                case Left:
+                    return ConstraintSet.LEFT;
+                case Top:
+                    return ConstraintSet.TOP;
+                case Right:
+                    return ConstraintSet.RIGHT;
+                case Bottom:
+                default:
+                    return ConstraintSet.BOTTOM;
+            }
+        }
+
+        public Edge opposite() {
+            switch (this) {
+                case Left:
+                    return Right;
+                case Top:
+                    return Bottom;
+                case Right:
+                    return Left;
+                case Bottom:
+                default:
+                    return Top;
+            }
         }
     }
 
@@ -94,7 +132,10 @@ public class EdgeFloatingActionButton extends FrameLayout {
             typedArray.recycle();
         }
 
+        padding = res.getDimensionPixelOffset(R.dimen.elementMargin); // FIXME: Likely temp until fetched live
+
         offsetEdge();
+        constrainOffsetEdge();
         cvMask.setRadius(cornerRadius);
 
         iv.setImageDrawable(image);
@@ -129,6 +170,23 @@ public class EdgeFloatingActionButton extends FrameLayout {
 
         cvMask.setLayoutParams(cvParams);
         flImage.setLayoutParams(flParams);
+    }
+
+    private void constrainOffsetEdge() {
+        ConstraintSet constraints = new ConstraintSet();
+        constraints.clone(clRoot);
+
+        if (Edge.Left.and(edge))   constrainTo(constraints, Edge.Left);
+        if (Edge.Top.and(edge))    constrainTo(constraints, Edge.Top);
+        if (Edge.Right.and(edge))  constrainTo(constraints, Edge.Right);
+        if (Edge.Bottom.and(edge)) constrainTo(constraints, Edge.Bottom);
+
+        constraints.applyTo(clRoot);
+    }
+
+    private void constrainTo(ConstraintSet constraints, Edge side) {
+        constraints.clear(space.getId(), side.opposite().constraintSide());
+        constraints.clear(flPadded.getId(), side.constraintSide());
     }
 
     @Override
